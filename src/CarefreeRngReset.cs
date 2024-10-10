63a0c1ab0a194827611ffafccf81775daeb29f68
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-10-10 20:33:50 +0200

    initial commit

---
+++ src/CarefreeRngReset.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using Modding;
+using UnityEngine.SceneManagement;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace CarefreeRngReset;
+
+[UsedImplicitly]
+public class CarefreeRngReset : Mod
+{
+    public override string GetVersion() => "in Hall of Gods";
+
+    public CarefreeRngReset() : base("Carefree RNG reset")
+    {}
+
+    public override void Initialize()
+    {
+        USceneManager.activeSceneChanged += OnActiveSceneChanged;
+    }
+
+    private void OnActiveSceneChanged(Scene arg0, Scene arg1)
+    {
+        if (arg1.name != "GG_Workshop") return;
+
+        var hc = HeroController.instance;
+        if (hc != null)
+        {
+            ReflectionHelper.SetField<HeroController, int>(hc, "hitsSinceShielded", 7);
+        }
+    }
+}
